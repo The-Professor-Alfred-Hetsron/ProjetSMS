c9c0854bff589674a3ec6d3676a1dd4bbc4f9dba
HerveDjomguem <hdjomguem@gmail.com>
2023-01-23 15:07:02 +0100

    modified:   Backend/API/Controllers/UserController.js 	modified:   Backend/package-lock.json 	modified:   Backend/package.json

--- Backend/API/Controllers/UserController.js
+++ Backend/API/Controllers/UserController.js
@@ -1,5 +1,6 @@
 // Same as ContactController
-
+const bcrypt = require('bcrypt');
+const jwt= require('jsonwebtoken');
 //mettre le chemin relatif vers le model user
 const User = require('');
 
@@ -8,17 +9,25 @@ const User = require('');
 exports.login =(req, res, next)=>{
   console.log('req.body',req.body)
     User.findOne({email: req.body.email})
-    .then(data=>{
-      console.log('data', data)
-      if(data === null){
+    .then(user=>{
+      console.log('user', user)
+      if(user === null){
           res.status(401).json({message:'identifiant ou mot de passe incorrecte'})
       }else{
-          bcrypt.compare(req.body.password,data.password)
+          bcrypt.compare(req.body.password,user.password)
           .then(valid=>{
               if(!valid){
                   res.status(401).json({message:'identifiant ou mot de passe incorrecte'})
               }else{
-                res.status(200).json({data});
+               //Implementation du token
+                res.status(200).json({
+                  userId: user._id,
+                  token: jwt.sign(
+                      {email: user.email, userId: user._id},
+                      'secret_this_should_be_longer',
+                      {expiresIn: '24h'}
+                  )
+                });
               }
           })
           .catch(error=>{
