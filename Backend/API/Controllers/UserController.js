b484230721485f971731563566265765925a0a58
HerveDjomguem <hdjomguem@gmail.com>
2023-01-23 14:32:02 +0100

    Update UserController.js

--- Backend/API/Controllers/UserController.js
+++ Backend/API/Controllers/UserController.js
@@ -1,51 +1,32 @@
-const bcrypt = require('bcrypt');
-const jwt= require('jsonwebtoken');
+// Same as ContactController
 
-const User = require('../models/User');
+//mettre le chemin relatif vers le model user
+const User = require('');
 
 
-exports.signup = (req, res, next) => {
-  bcrypt.hash(req.body.password,10)
-  .then(hash=>{
-    const user = new User({
-        email: req.body.email,
-        password: hash
-    });
-    user.save()
-    .then(()=> res.status(201).json({message: 'Utilisateur crée !'}))
-    .catch(error =>res.status(500).json({error}));
-  })
-  .catch(error =>res.status(500).json({error}));
-
-};
 
 exports.login =(req, res, next)=>{
-  User.findOne({email: req.body.email})
-  .then(user=>{
-    if(user === null){
-        res.status(401).json({message:'Paire identifiant/mot de passe incorrecte'})
-    }else{
-        bcrypt.compare(req.body.password,user.password)
-        .then(valid=>{
-            if(!valid){
-                res.status(401).json({message:'Paire identifiant/mot de passe incorrecte'})
-            }else{
-              res.status(200).json({
-                userId: user._id,
-                token: jwt.sign(
-                    {userId: user._id},
-                    'RANDOM_TOKEN_SECRET',
-                    {expiresIn: '24h'}
-                )
-              });
-            }
-        })
-        .catch(error=>{
-            res.status(500).json({error});
-        })
-     }
-  })
-  .catch(error =>{
-    res.status(500).json({error});
-  })
-};
+  console.log('req.body',req.body)
+    User.findOne({email: req.body.email})
+    .then(data=>{
+      console.log('data', data)
+      if(data === null){
+          res.status(401).json({message:'identifiant ou mot de passe incorrecte'})
+      }else{
+          bcrypt.compare(req.body.password,data.password)
+          .then(valid=>{
+              if(!valid){
+                  res.status(401).json({message:'identifiant ou mot de passe incorrecte'})
+              }else{
+                res.status(200).json({data});
+              }
+          })
+          .catch(error=>{
+              res.status(500).json({error});
+          })
+       }
+    })
+    .catch(error =>{
+      res.status(500).json({error});
+    })
+  };
