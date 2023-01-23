c4770eb69ee44e2687a9fac6b627b57ba82d9440
Adzaba3 <adzabaarbogaste@gmail.com>
2023-01-23 02:16:05 +0200

    forgot email page

--- Frontend/projet-sms/src/app/app.module.ts
+++ Frontend/projet-sms/src/app/app.module.ts
@@ -1,44 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ContactsPageComponent } from 'src/Routes/contacts-page/contacts-page.component';
+import { ForgotPasswordPageComponent } from 'src/Routes/forgot-password-page/forgot-password-page.component';
+import { LoginPageComponent } from 'src/Routes/login-page/login-page.component';
+import { MessagesPageComponent } from 'src/Routes/messages-page/messages-page.component';
+import { SaveContactsPageComponent } from 'src/Routes/save-contacts-page/save-contacts-page.component';
+import { SendSmsPageComponent } from 'src/Routes/send-sms-page/send-sms-page.component';
+import { SigninPageComponent } from 'src/Routes/signin-page/signin-page.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { CreationDeCompteComponent } from './creation-de-compte/creation-de-compte.component';
-import { RecupMotDePasseComponent } from './recup-mot-de-passe/recup-mot-de-passe.component';
-import { EnregManuelDeContactsComponent } from './enreg-manuel-de-contacts/enreg-manuel-de-contacts.component';
-import { CreateAccountComponent } from './create-account/create-account.component';
 import { ForgetPasswordComponent } from './forget-password/forget-password.component';
-import { SaveContactsComponent } from './save-contacts/save-contacts.component';
-import { ImportContactsComponent } from './import-contacts/import-contacts.component';
-import { ContactsPageComponent } from '../Routes/contacts-page/contacts-page.component';
-import { LoginPageComponent } from './login-page/login-page.component';
-import { SigninPageComponent } from './signin-page/signin-page.component';
-import { ForgotPasswordPageComponent } from './forgot-password-page/forgot-password-page.component';
-import { SaveContactsPageComponent } from './save-contacts-page/save-contacts-page.component';
-import { ImportContactsPageComponent } from './import-contacts-page/import-contacts-page.component';
-import { SendSmsPageComponent } from './send-sms-page/send-sms-page.component';
-import { MessagesPageComponent } from './messages-page/messages-page.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    CreationDeCompteComponent,
-    RecupMotDePasseComponent,
-    EnregManuelDeContactsComponent,
-    CreateAccountComponent,
-    ForgetPasswordComponent,
-    SaveContactsComponent,
-    ImportContactsComponent,
-    ContactsPageComponent,
+    MessagesPageComponent,
     LoginPageComponent,
-    SigninPageComponent,
     ForgotPasswordPageComponent,
     SaveContactsPageComponent,
-    ImportContactsPageComponent,
     SendSmsPageComponent,
-    MessagesPageComponent
+    SigninPageComponent,
+    ContactsPageComponent
   ],
   imports: [
     BrowserModule,
